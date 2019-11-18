51b6d6613212b28e47f1674b07e1c6728dcea625
TripleM <therealtriplem@gmx.de>
2019-11-18 19:16:52 +0100

    Gespräch mit Haslach eingearbeitet

--- Mathlab Fast/VisualisationTest.m
+++ Mathlab Fast/VisualisationTest.m
@@ -25,7 +25,7 @@ text(xtips1,ytips1,labels1,'HorizontalAlignment','center',...
  title('Fehleranzahl in Abhänigkeit der Mikrofonpositionen');
 ylabel('Fehleranzahl');
 %}
-
+%{
 x = categorical({'Schätzwert1','Schätzwert2', 'Schätzwert3', 'Schätzwert4','Schätzwert5'});
 y = [41,138,449,494,7438];
 b = bar(x,y);
@@ -39,3 +39,37 @@ text(xtips1,ytips1,labels1,'HorizontalAlignment','center',...
 
  title('Fehleranzahl in Abhänigkeit des Schätzwertes');
 ylabel('Fehleranzahl');
+%}
+%{
+
+
+%x = categorical({'350','750', '1000', '1500','2000','2500','3000','3500'});
+x = [350,750,1000,1500,2000,2500,3000,3500];
+y = [36.1,26.3,21.9,10.9,1.8,1.1,0.3,0.03];
+b = bar(x,y);
+
+xtips1 = b(1).XEndPoints;
+ytips1 = b(1).YEndPoints;
+labels1 = string(b(1).YData);
+text(xtips1,ytips1,labels1,'HorizontalAlignment','center',...
+    'VerticalAlignment','bottom')
+
+
+ title('Relative Fehlerhäufigkeit in Abhängigkeit von K_{corr}');
+ylabel('Fehleranzahl [%]');
+xlabel('K_{corr}');
+%}
+
+plot(xcorr(mic2sigMain, corrSegment))
+title('Korrelation');
+
+
+
+%{
+x = [1:260002];
+x = x/samplingRate;
+x= x';
+plot(x',Qt);
+xlabel('Laufzeit [s]');
+title('Auszug aus Moskitosignal');
+%}
